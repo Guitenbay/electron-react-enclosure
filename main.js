4d3527eb222feaa1102d50bebfd943e4e1c7fc29
Guitenbay <gtenbay@163.com>
2020-07-22 14:39:30 +0800

    添加下载提示

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /* main.js */
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
+
+const title = '教师综合考察记录系统';
 
 let mainWindow = null;
 const createWindow = () => {
@@ -24,6 +26,31 @@ const createWindow = () => {
       ? 'http://localhost:8000'
       :  path.join(__dirname, "/build/index.html");
   mainWindow.loadURL(startUrl);
+
+  // 控制下载文件
+  mainWindow.webContents.session.on('will-download', (event, item, webContents) => {
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        console.log(dialog.showMessageBoxSync({ type: 'info', title, message: 'Download is interrupted but can be resumed'}));
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          console.log(dialog.showMessageBoxSync({ type: 'info', title, message: 'Download is paused'}));
+        } else {
+          // 显示下载进度
+          mainWindow.setProgressBar(item.getReceivedBytes() / item.getTotalBytes());
+        }
+      }
+    })
+    item.once('done', (event, state) => {
+      if (state === 'completed') {
+        console.log(dialog.showMessageBoxSync({ type: 'info', title, message: '下载完成！'}));
+      } else {
+        console.log(dialog.showMessageBoxSync({ type: 'error', title, message: `下载失败：${state}`}));
+      }
+      // 不显示下载进度
+      mainWindow.setProgressBar(-1);
+    })
+  });
 
   mainWindow.on('closed', function () {
     mainWindow = null;
